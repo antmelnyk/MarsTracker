1c9eb35bb89229221c79efcfc2ade63918e2498c
pixelgoo <pixelgoo@gmail.com>
2016-12-04 18:23:15 +0200

    Resolving version display issue with file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,14 @@
 class WelcomeController < ApplicationController
+
   def index
-    @version = AppConfig.git_version
+    deployed = false
+    if deployed
+      @version = File.read("version.txt")
+    else
+      out_file = File.new("version.txt", "w")
+      g = Git.open(Rails.root, :log => Logger.new(STDOUT))
+      out_file.puts(@version = g.describe)
+      out_file.close
+    end
   end
 end
