0958f3919e345b5171640fb51b1649b06c2ccfbd
sedin1942 <sedin1942@yahoo.com>
2016-12-04 20:31:09 +0200

    registration form testing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+﻿require 'spec_helper'
 
 describe "Перегляд статичних сторінок" do
 
@@ -17,4 +17,13 @@ describe "Перегляд статичних сторінок" do
 	end
 end
   
+  describe "Відображення форми" do
+it "форма має бути присутня для заповнення" do
+		visit root_path
+		click_link('register')
+fill_in 'login', :with => 'testlogin'
+fill_in 'password', :with => 'testpassword'
+end
+end
+
 end
