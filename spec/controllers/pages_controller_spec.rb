29a8d508362899bf61c4823f98c86270e8ca7253
pixelgoo <pixelgoo@gmail.com>
2016-12-08 13:52:01 +0200

    PagesController test fixed

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe PagesController, :type => :controller do
   it "redirects to 404 page if page does't exist" do
-    visit('/pages/test')
-    expect(response.status).to eq(200)
+    get :show, { controller: 'pages', page: 'not_existing_page' }
+    expect(response.status).to eq(404)
   end
 
   it "renders mars tracker page" do
