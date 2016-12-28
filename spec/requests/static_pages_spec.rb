1d2d5f9f6d1250679697b7f130ce07e279b3d5f2
goodlikemax <Pidlisnyi.post@gmail.com>
2016-12-28 15:58:01 +0200

    Add test for db articles extraction

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,20 +1,49 @@
-require 'spec_helper'
+﻿require 'spec_helper'
+
+
 
 describe "Перегляд статичних сторінок" do
 
-  describe "Головна сторінка" do
-    it "повинна відображати версію продукту" do
-      visit root_path
-      expect(page).to have_content('Версия проекта')
-    end
-  end
-
-  describe "Перехід на статті" do
-	  it "повинен відображати всі статті" do
-		  visit root_path
-		  click_link('articles-link')
-      page.has_css?('table')
-    end
-  end
   
+	describe "Головна сторінка" do
+    
+		it "повинна відображати версію продукту" do
+      
+			visit root_path
+
+			expect(page).to have_content('Версия проекта')
+    
+		end
+  
+	end
+
+  
+	describe "Перехід на статті" do
+
+	  it "повинен відображати всі статті" do
+
+		  visit root_path
+
+		  click_link('articles-link')
+
+      		  page.has_css?('table')
+    
+	  end
+
+  	end
+
+
+
+	describe "Опис проектів" do
+
+	  it "повинен відображати статті з бази даних" do
+
+		  visit root_path
+      		  expect(page).to have_content('Басом Лансдорпом')
+      		  expect(page).to have_content('Илоном Маском')      
+	  end
+
+  	end
+
+
 end
