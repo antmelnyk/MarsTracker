024441e5284a924dbeb37422367d20ed571bd335
pixelgoo <pixelgoo@gmail.com>
2016-12-28 20:59:48 +0200

    fix UsersController mailer issue #3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,13 +15,15 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    if @user.save
-      UserMailer.welcome_email(@user).deliver
-      format.html { redirect_to @user, notice: 'User was successfully created.' }
-      format.json { render json :show, status: :created, location: @user }
-    else
-      format.html { render action :new }
-      format.json { render json: @user.errors, status: :unprocessable_entity }
+    respond_to do |format|
+      if @user.save
+        UserMailer.welcome_email(@user).deliver
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json :show, status: :created, location: @user }
+      else
+        format.html { render action :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
