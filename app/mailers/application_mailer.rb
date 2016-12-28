c99612654c4e200f08d8dd4f2e578d234dd1e945
pixelgoo <pixelgoo@gmail.com>
2016-12-28 20:28:39 +0200

    switch to gmail mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "pixelgoo@gmail.com"
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,11 +3,13 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address              => "smtp.mailgun.org",
+      :address              => "smtp.gmail.com",
       :port                 => 587,
-      :user_name            => ENV['username'],
-      :password             => ENV['password'],
+      :user_name            => ENV['gmail_username'],
+      :password             => ENV['gmail_password'],
       :authentication       => "plain",
+      :enable_starttls_auto => true
+
   }
 
   # Code is not reloaded between requests.
