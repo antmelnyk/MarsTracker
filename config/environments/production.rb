b29e3ab63f51783ba1173fe79d61e922019751b4
pixelgoo <pixelgoo@gmail.com>
2016-12-28 20:20:31 +0200

    remove domain config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,6 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address              => "smtp.mailgun.org",
       :port                 => 587,
-      :domain               => ENV['domain'],
       :user_name            => ENV['username'],
       :password             => ENV['password'],
       :authentication       => "plain",
